94e178d4e332f8c5b53362cdb20c8901dc19c443
sebastien Amafou <sebastien.amafou@gmail.com>
2025-12-15 01:06:32 +0100

    Correction Crash YAML et Ajout vercel.json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -8,31 +8,30 @@ import { Pharmacie } from './pharmacie/pharmacie.entity';
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    // On injecte l'outil pour parler à la table Pharmacie
     @InjectRepository(Pharmacie)
     private pharmacieRepo: Repository<Pharmacie>,
   ) {}
 
+  // ✅ Redirection Automatique : La racine (/) renvoie vers le login Pro
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Redirect('/auth/web/login', 302)
+  root() {
+    // Cette méthode ne fait rien d'autre que rediriger
+    return;
   }
 
-  // Voici notre URL de test
+  // (Optionnel) Votre ancienne route de test, gardée au cas où
   @Get('creer-test')
   async creerPharmacieTest() {
-    // On crée une fausse pharmacie au Plateau
     const nouvellePharmacie = this.pharmacieRepo.create({
       nom: 'Grande Pharmacie du Plateau',
       adresse: 'Avenue Chardy, Abidjan',
       estDeGarde: true,
       position: {
         type: 'Point',
-        coordinates: [-4.0197, 5.3261], // Longitude, Latitude (Abidjan)
+        coordinates: [-4.0197, 5.3261], 
       },
     });
-
-    // On sauvegarde dans la base
     return await this.pharmacieRepo.save(nouvellePharmacie);
   }
 }
